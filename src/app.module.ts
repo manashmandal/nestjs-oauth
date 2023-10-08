c6e5ba45e9beca6fac7ab25fd112fc64e57174d3
manashmandal <manashmndl@gmail.com>
2023-10-08 04:06:52 +0200

    fix cors error

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { PassportModule } from '@nestjs/passport';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'dispisdis',
+      username: 'root',
       password: 'admin',
       database: 'google_oauth2_app',
       entities: [User],
